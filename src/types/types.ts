ce93b5ccc36c6b026b1e93e94f3c9440a704f3c2
windowdotonload <275981471@qq.com>
2021-07-01 17:14:07 +0800

    mod

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,22 +1,31 @@
 /*
+ * @Descripttion: 
+ * @version: 
+ * @Author: windowdotonload
+ */
+/*
  * @Descripttion:
  * @version:
  * @Author: windowdotonload
  */
+
+
 export interface userProps {
     isLogin: boolean;
     name?: string;
     id?: number;
-}
+};
 
 export interface columnProps {
     id: number;
     title: string;
     avatar?: string;
     description: string;
-}
+};
 
 export interface ruleProp {
     type: 'required' | 'email'
     message: string
-}
+};
+
+
